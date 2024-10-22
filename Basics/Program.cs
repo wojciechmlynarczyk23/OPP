a2947049062bb219841563b819eddc27bce3032c
JanAndrzejczak <jan.andrzejczak@uczen.zsk.poznan.pl>
2024-10-22 13:08:25 +0200

    modify file

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+         class Group(string name, int count, string mainTeacher, Array students)
+        {
+            private string name = name;
+            private int count = count;
+            private string mainTeacher = mainTeacher;
+            private Array students = students;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Group g1 = new Group("5c",20,"Obsti");
+           Group g2 = new Group("5b", 24, "Radar");
+
+            Random rnd = new Random();
+            int wynik = rnd.Next(1, 3);
+
+            if (wynik == 1)
+            {
+                Console.WriteLine("wygrywa " + g1.mainTeacher + " z klasy " + g1.name);
+            }
+            else if (wynik == 2)
+            {
+                Console.WriteLine("wygrywa " + g2.mainTeacher + " z klasy " + g2.name);
+            }
         }
     }
 }
